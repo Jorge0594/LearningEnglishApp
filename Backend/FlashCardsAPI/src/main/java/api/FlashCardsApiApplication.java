2b9e140008af7991bc2f263bee458d978f912c43
Jorge0594 <jsm0594@gmail.com>
2019-03-23 12:49:01 +0100

    API created

---
+++ Backend/FlashCardsAPI/src/main/java/api/FlashCardsApiApplication.java
@@ -0,0 +1,13 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardsApiApplication.class, args);
+	}
+
+}
