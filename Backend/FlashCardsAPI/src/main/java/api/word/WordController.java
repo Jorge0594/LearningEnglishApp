932d284c7a34563c1d86023da745c2819e8dbcd2
Jorge0594 <jsm0594@gmail.com>
2019-03-24 13:04:31 +0100

    Add new query
    This query permits to find a word with its subType

--- Backend/FlashCardsAPI/src/main/java/api/word/WordController.java
+++ Backend/FlashCardsAPI/src/main/java/api/word/WordController.java
@@ -3,9 +3,13 @@ package api.word;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +22,8 @@ public class WordController {
 	
 	@Autowired
 	private WordRepository wordRepository;
+	@Autowired
+	private MongoTemplate mongoTemplate;
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public ResponseEntity<Word> addWord(@RequestBody Word word){
@@ -35,6 +41,13 @@ public class WordController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ResponseEntity<List<Word>> getAllWords(){
 		return new ResponseEntity<List<Word>>(wordRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/subtype/{subtype}", method = RequestMethod.GET)
+	public ResponseEntity<List<Word>> findBySubtype(@PathVariable String subtype){
+		Query query = new Query();
+		query.addCriteria(Criteria.where("subType").is(subtype));
+		return new ResponseEntity<List<Word>>(mongoTemplate.find(query, Word.class), HttpStatus.OK);
 	}
 
 }
